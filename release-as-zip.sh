19b88ef9e73089bad77d68d233e4e811c24774b0
Hajo Eichler <hajo.eichler@commercetools.de>
2014-01-31 08:41:47 +0100

    Use --prodution to get dependencies.

--- release-as-zip.sh
+++ release-as-zip.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-rm -rf lib
-
 BRANCH_NAME='latest'
+
+grunt build
+rm -rf node_modules
 
 npm version patch
 git checkout ${BRANCH_NAME}
 git merge master
 
-grunt build
+rm -rf node_modules
+npm install --production
 git add -f lib/
-libs=$(cat package.json | jq -r '.dependencies' | grep ':' | cut -d: -f1 | tr -d " " | tr -d '"')
-for lib in $libs; do
-    git add -f node_modules/$lib
-done
+git add -f node_modules/
 git commit -m "Update generated code and runtime dependencies."
 git push origin ${BRANCH_NAME}
 
 git checkout master
 npm version patch
 git push origin master
+npm install
