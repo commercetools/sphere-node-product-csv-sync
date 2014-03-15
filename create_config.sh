1f99b422fef7da9c353f1f7d2292e7b0c5317dfc
Nicola Molinari <nicola.molinari@commercetools.de>
2014-03-15 12:58:38 +0100

    Remove trailing comma

--- create_config.sh
+++ create_config.sh
@@ -5,7 +5,7 @@ cat > "config.js" << EOF
 exports.config = {
   client_id: "${SPHERE_CLIENT_ID}",
   client_secret: "${SPHERE_CLIENT_SECRET}",
-  project_key: "${SPHERE_PROJECT_KEY}",
+  project_key: "${SPHERE_PROJECT_KEY}"
 }
 
 exports.logConfig = {
